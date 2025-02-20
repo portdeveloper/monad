e2e9cf64faea06fe363c2bc24795b8236f973995
port <108868128+portdeveloper@users.noreply.github.com>
2025-02-20 22:25:26 +0300

    Change how the extension works (thx shiv ily)

---
+++ extension/packages/nextjs/utils/scaffold-eth/otherChains.ts
@@ -0,0 +1,17 @@
+export const monad = {
+    id: 10143,
+    name: "Monad Testnet",
+    nativeCurrency: { name: "Monad", symbol: "DMON", decimals: 18 },
+    rpcUrls: {
+      default: {
+        http: ["https://testnet-rpc2.monad.xyz/52227f026fa8fac9e2014c58fbf5643369b3bfc6"],
+      },
+    },
+    blockExplorers: {
+      default: {
+        name: "Monad Testnet Explorer",
+        url: "https://testnet.monadexplorer.com/",
+      },
+    },
+  };
+  
